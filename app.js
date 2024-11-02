b4b0862c508b7cb42683642aa1fefb16edbc2bb3
Chayma <114072354+Chayma-lab@users.noreply.github.com>
2024-11-02 12:11:46 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const http = require('http');
+import { createServer } from 'http';
 const port = process.env.PORT || 3000;
 
 const requestHandler = (request, response) => {
   response.end('Hello World from AWS Elastic Beanstalk!');
 };
 
-const server = http.createServer(requestHandler);
+const server = createServer(requestHandler);
 
 server.listen(port, () => {
   console.log(`Server running on port ${port}`);
